815419d25a2de83272e39cb23325bc5acaf142e7
root <bfmvtm@gmail.com>
2018-02-09 20:02:43 -0400

    Add files

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    Congratulations! You are now an admin!
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
